d00cd3338e1497390e626a72c0d7c82b06fed8eb
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2022-03-20 16:46:04 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const authroute = require('./router/auth')
+const userroute = require('./router/user')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+  
+app.use('/auth',authroute)  
+app.use('/user',userroute)
+
+app.use((req,res,next)=>{
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+ })
+  
+ app.use((err,req,res,next)=>{
+    res.status(err.status || 500)
+    res.json({
+       error:{
+          message:err.message
+       }
+    })
+ })
+
+mongoose.connect(port,()=>console.log('connected to db'))
+
+
+app.listen(port, ()=>console.log(`app running on port ${port}`))
